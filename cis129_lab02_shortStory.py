48fc053e4ad5783287a50980e8dfdf1e8f6140dc
Kyllab <134439563+Kyllab@users.noreply.github.com>
2025-02-19 21:26:26 -0700

    Create cis129_lab02_shortStory.py
    print('Hello, World!')
    
    Scroll down to the "Commit new file" section.
    Enter a commit message, for example, "Add initial cis129_lab02_shortStory.py." A commit message should be informative and concise, describing what this commit accomplished.
    Click the "Commit new file" button.

---
+++ cis129_lab02_shortStory.py
@@ -0,0 +1,5 @@
+print('Hello, World!')
+
+Scroll down to the "Commit new file" section.
+Enter a commit message, for example, "Add initial cis129_lab02_shortStory.py." A commit message should be informative and concise, describing what this commit accomplished.
+Click the "Commit new file" button.
