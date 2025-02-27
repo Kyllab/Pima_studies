2f76a17b6bf78bf79d2f917050f828a95f468a8c
Kyllab <134439563+Kyllab@users.noreply.github.com>
2025-02-26 21:37:39 -0700

    Add files via upload

---
+++ Coffee_shop_CIS.py
@@ -0,0 +1,125 @@
+Python 3.10.5 (v3.10.5:f377153967, Jun  6 2022, 12:36:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+
+
+
+
+
+Script "cis129_lab03_coffeeShop.py".
+SyntaxError: invalid syntax
+//Module_3_Lab: Display name "cis129_lab03_coffeeShop.py"
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+//"""Module_3_Lab: Constant:cis129_lab03_coffeeShop.py"""//
+SyntaxError: invalid syntax
+
+MAIN
+//Declare variables and constants for this program
+//Input: Get input from the customer
+//Processing: Calculate the total cost
+//Output: Display the receipt to the customer
+End Main
+
+Main
+//Display"**************************************"
+  Display "Hello Welcome To My Coffee and Muffin Shop"
+  Declare string nameOfCroissant
+  Declare string nameOfItemCoffees 
+  declare string nameOfMuffins 
+  Declare string nameOfApple 
+  Declare Integer OfItemsPurchased
+  
+  Declare Real priceOfItem
+  Declare Real priceOfCroissant  2.50
+  Declare Real priceOfCoffees    5.00
+  declare Real priceOfMuffins    4.00
+  declare Real priceOfApple       .50
+  Declare Integer numberOfItemsPurchased
+  Declare Real priceOfItem
+
+//Input:Get input from customer
+  Display "what itmes are you purchasing?"
+  Input NameOfItem
+  Dislay "Please enter the number of items brought"
+  Input numberOfItemsPurchased
+  Display "Please enter the price of the item"
+  Input priceOfItem
+  totalCost = numberOfItemsPurchased * priceOfItem
+
+//Display the receipt to the customer
+
+  Display"***************************************"
+
+***************************************
+  Display "------ Receipt ------"
+  Display "Item Purchased: ", nameOfItem
+  Display "Number purchased: ", numberOfItemsPurchased
+  Display "Price of Each Item: ", priceOfItem
+  Display "Total Cost: ", totalCost
+
+//Decl1 Coffee at 5.00 each:  5.00
+  2 Muffins at 4.00 each:  8.00
+  Declare Constant COUNTY_TAX_AMOUNT =.06 tax:  .78
+  Display firstNbr, " * ", secondNbr, " equals ", totalotal: 13.78
+
+  Display "Thank you and have A Great Day!"
+  Display "My Coffee and Muffin Shop Receipt"
+  Display "***************************************"
+  
+SyntaxError: multiple statements found while compiling a single statement
+MAIN
+//Declare variables and constants for this program
+//Input: Get input from the customer
+//Processing: Calculate the total cost
+//Output: Display the receipt to the customer
+End Main
+
+Main
+//Display"**************************************"
+  Display "Hello Welcome To My Coffee and Muffin Shop"
+  Declare string nameOfCroissant
+  Declare string nameOfItemCoffees
+  declare string nameOfMuffins
+  Declare string nameOfApple
+  Declare Integer OfItemsPurchased
+
+  Declare Real priceOfItem
+  Declare Real priceOfCroissant  2.50
+  Declare Real priceOfCoffees    5.00
+  declare Real priceOfMuffins    4.00
+  declare Real priceOfApple       .50
+  Declare Integer numberOfItemsPurchased
+  Declare Real priceOfItem
+
+//Input:Get input from customer
+  Display "what itmes are you purchasing?"
+  Input NameOfItem
+  Dislay "Please enter the number of items brought"
+  Input numberOfItemsPurchased
+  Display "Please enter the price of the item"
+  Input priceOfItem
+  totalCost = numberOfItemsPurchased * priceOfItem
+
+//Display the receipt to the customer
+
+  Display"***************************************"
+
+***************************************
+  Display "------ Receipt ------"
+  Display "Item Purchased: ", nameOfItem
+  Display "Number purchased: ", numberOfItemsPurchased
+  Display "Price of Each Item: ", priceOfItem
+  Display "Total Cost: ", totalCost
+
+//Decl1 Coffee at 5.00 each:  5.00
+  2 Muffins at 4.00 each:  8.00
+  Declare Constant COUNTY_TAX_AMOUNT =.06 tax:  .78
+  Display firstNbr, " * ", secondNbr, " equals ", totalotal: 13.78
+
+  Display "Thank you and have A Great Day!"
+  Display "My Coffee and Muffin Shop Receipt"
+  Display "***************************************"
+  
+SyntaxError: multiple statements found while compiling a single statement
